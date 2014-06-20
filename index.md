454a74b0b630826c387cc1e1d4d7ba7776736e1c
stujo <stujo@imagescript.com>
2014-06-20 16:49:57 -0700

    Insecure Lunch Options

---
+++ index.md
@@ -0,0 +1,14 @@
+Meteor
+==========
+
+* [Meteor](http://www.meteor.com/)
+* ```curl https://install.meteor.com/```
+
+
+#Notes
+* Ships with ```spacebars``` for templates
+* Helpers are the functions which provide data to templates
+
+
+
+
---
+++ lunch/lunch.js
@@ -0,0 +1,47 @@
+Restaurants = new Meteor.Collection("restaurants");
+
+if (Meteor.isClient) {
+
+  // Helper Function available in template
+  Template.restaurants.restaurants = function () {
+    return Restaurants.find({}, {sort: {score: -1, name: 1}});
+  };
+
+  Template.restaurant.selected = function () {
+    return Session.equals("selected_restaurant_id", this._id) ? "selected" : "";
+  };
+
+  Template.restaurants.selected_restaurant = function () {
+    if (Session.get("selected_restaurant_id")) {
+      return Restaurants.findOne(Session.get("selected_restaurant_id"));
+    }
+  };
+
+  Template.restaurants.events({
+      'click': function () {
+        if (this._id != Session.get("selected_restaurant_id")) {
+          if (Session.get("selected_restaurant_id")) {
+            Restaurants.update(Session.get("selected_restaurant_id"), {$inc: {score: -5}});
+          }
+          Session.set("selected_restaurant_id", this._id);
+
+          if (Session.get("selected_restaurant_id")) {
+            Restaurants.update(Session.get("selected_restaurant_id"), {$inc: {score: 5}});
+          }
+        }
+      }
+    }
+  );
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+    if (Restaurants.find().count() === 0) {
+      var locs = ["Brickhouse", "HRD", "Centro", "Garaje"];
+      for (var i = 0; i < locs.length; i++) {
+        Restaurants.insert({name: locs[i], score: 0});
+      }
+    }
+  });
+}
